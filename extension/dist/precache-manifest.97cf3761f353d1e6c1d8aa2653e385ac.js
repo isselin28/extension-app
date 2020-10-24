9cf0349627c2eba55374b06caab0375a7bcd45d1
isselin28 <isselin@mokapos.com>
2020-10-24 15:25:02 +0800

    install styled components

---
+++ extension/dist/precache-manifest.97cf3761f353d1e6c1d8aa2653e385ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c4e8ec6f83b510c07d4cddcfb9eb9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10bfa666e528e7ec3f1d",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "e7f0ae97edb573589af7",
+    "url": "/static/js/2.fb5e9efb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb5e9efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bfa666e528e7ec3f1d",
+    "url": "/static/js/main.70a15f74.chunk.js"
+  },
+  {
+    "revision": "af3e91129ec339b78669",
+    "url": "/static/js/runtime-main.d0da8deb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- extension/src/App.js
+++ extension/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hey I'm reloaded.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn What
         </a>
       </header>
     </div>
